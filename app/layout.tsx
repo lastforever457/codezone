f2522c03bf8d69c13e3b716808f92768f693f3da
lastforever457 <lastforever457@gmail.com>
2024-11-23 19:01:33 +0500

    fix

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,5 @@
 'use client';
 
-import { ConfigProvider } from 'antd';
 import { ReactNode } from 'react';
 import '../i18n/i18n';
 import './globals.css';
@@ -12,9 +11,7 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body className="antialiased">
-        <ConfigProvider>{children}</ConfigProvider>
-      </body>
+      <body className="antialiased">{children}</body>
     </html>
   );
 }
