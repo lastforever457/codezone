1207a79811536a769d39560c1865fbf64f4dad56
lastforever457 <lastforever457@gmail.com>
2024-11-23 19:20:08 +0500

    fix

--- i18n/i18n.ts
+++ i18n/i18n.ts
@@ -1,22 +1,21 @@
 import i18n from 'i18next';
-import LanguageDetector from 'i18next-browser-languagedetector';
-import HttpApi from 'i18next-http-backend';
 import { initReactI18next } from 'react-i18next';
 
-i18n
-  .use(HttpApi)
-  .use(LanguageDetector)
-  .use(initReactI18next)
-  .init({
-    supportedLngs: ['uz', 'ru'],
-    fallbackLng: 'uz',
-    detection: {
-      order: ['path', 'cookie', 'localStorage', 'navigator'],
-      caches: ['cookie'],
+i18n.use(initReactI18next).init({
+  supportedLngs: ['uz', 'ru'],
+  fallbackLng: 'uz',
+  detection: {
+    order: ['path', 'cookie', 'localStorage', 'navigator'],
+    caches: ['cookie'],
+  },
+  resources: {
+    uz: {
+      translation: require('./locales/uz/translation.json'),
     },
-    backend: {
-      loadPath: '/locales/{{lng}}/translation.json',
+    ru: {
+      translation: require('./locales/ru/translation.json'),
     },
-  });
+  },
+});
 
 export default i18n;
