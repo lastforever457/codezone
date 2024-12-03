583cdbe0e7f5191f00f723d18d075ab95c085741
lastforever457 <lastforever457@gmail.com>
2024-12-03 23:21:00 +0500

    fix: translations config

--- next.config.ts
+++ next.config.ts
@@ -1,11 +1,9 @@
 import type { NextConfig } from 'next';
-import { i18n } from './next-i18next.config';
 
 const nextConfig: NextConfig = {
   devIndicators: {
     buildActivity: false,
   },
-  i18n, // Add i18n configuration here
 };
 
 export default nextConfig;
